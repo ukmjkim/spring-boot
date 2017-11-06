72198200f253a131b0ba6ac35af3200efb54d345
Mijung Kim <MijungKimMacPro@Mijungs-MacBook-Pro-2.local>
2017-11-05 21:09:38 -0800

    spring boot1 migration to boot2

---
+++ spring-boot-web-application/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into TODO values (1001, 'Learn how to play the guitar', false, sysdate(), 'mjkim');
+insert into TODO values (1002, 'Learn how to play the piano', false, sysdate(), 'mjkim');
+insert into TODO values (1003, 'Learn how to play the flute', false, sysdate(), 'mjkim');
+insert into TODO values (1004, 'Learn how to play the trumpbon', false, sysdate(), 'mjkim');
+insert into TODO values (1005, 'Learn how to play the drum', false, sysdate(), 'mjkim');
+insert into TODO values (1006, 'Learn how to play the keyboard', false, sysdate(), 'mjkim');
+insert into TODO values (1007, 'Learn how to play the harp', false, sysdate(), 'mjkim');
+insert into TODO values (1008, 'Learn how to play the recorder', false, sysdate(), 'mjkim');
+
